6027dc4885a8ce5e5df5215e2866f91df5c24e02
George Stephen <georgesteve561@gmail.com>
2025-01-25 16:20:49 +0300

    New Api for my system

---
+++ src/main/java/com/infinie/alpha_estate_api/AlphaEstateApiApplication.java
@@ -0,0 +1,13 @@
+package com.infinie.alpha_estate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaEstateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaEstateApiApplication.class, args);
+	}
+
+}
